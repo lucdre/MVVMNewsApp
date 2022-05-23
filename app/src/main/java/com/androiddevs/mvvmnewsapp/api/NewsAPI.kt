c16c840a1669ad7c2beb5e3c8c26897293354c41
Lucdre <lucadanielrex95@gmail.com>
2022-05-23 23:14:03 +0200

    Added Breaking News

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
@@ -11,7 +11,7 @@ interface NewsAPI {
     @GET("v2/top-headlines")
     suspend fun getBreakingNews(
         @Query("country")
-        countryCode: String = "es",
+        countryCode: String = "de",
         @Query("page")
         pageNumber: Int = 1,
         @Query("apiKey")
